3546230156e3a7e75c41156393525ffa66d01fa1
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-04-06 17:46:42 +0300

    Написал метод для получения информации по id вакансии

--- src/HeadHunterAPI.py
+++ src/HeadHunterAPI.py
@@ -13,9 +13,9 @@ class HeadHunterApi(ParseCLS):
         self.__error_logs = []
 
         self.__params = {"text": args[:],
-                       "page": 1,
-                       "per_page": 20
-                       }
+                         "page": 1,
+                         "per_page": 20
+                         }
 
         self.__response = requests.get(url=self.__base_url, params=self.__params)
         if self.__response.status_code == 200:
@@ -54,9 +54,10 @@ class HHVacancyInterface:
                 if i['salary']['from'] is None:
                     i['salary']['from'] = "Начальная з/п не указана"
 
-                self.__list_of_vacancies.append(f"Наименование вакансии: {i['name']}. "
-                                                f"Месторасположение: {i['area']['name']}. "
+                self.__list_of_vacancies.append(f"ID вакансии: {i['id']}. "
+                                                f"Наименование вакансии: {i['name']}. "
                                                 f"Заработная плата: {i['salary']['from']} - {i['salary']['to']}. "
+                                                f"Месторасположение: {i['area']['name']}. "
                                                 f"Ссылка на вакансию: {i['alternate_url']}.")
             self.__list_of_vacancies.append(f"Время формирования запроса: {self.__current_time}")
 
@@ -97,20 +98,48 @@ class HHVacancyInterface:
 
             for part in part_of_dict:
                 key_value = part.split(': ')
-                vacancy_dict[key_value[0]] =key_value[1]
+                vacancy_dict[key_value[0]] = key_value[1]
             result_data.append(vacancy_dict)
 
         return result_data
 
+    def get_full_information_by_id(self, id: str | int):
 
+        result_info = []
+
+        if len(self.__list_of_vacancies) == 0:
+            return 'Список вакансий пуст, чтобы заполнить его, воспользуйтесь методом "fill_vacancy_list"'
+
+        for i in self.__hh_instance.generate_vacancy():
+
+            if i['salary']['to'] is None:
+                i['salary']['to'] = "Максимальный порог не указан"
+
+            if i['salary']['from'] is None:
+                i['salary']['from'] = "Начальная з/п не указана"
+
+            if i['id'] == str(id):
+                if i['address'] is None:
+
+                    result_info.append(f"Наименование вакансии: {i['name']}. "
+                                       f"Заработная плата({i['salary']['currency']}): {i['salary']['from']} - {i['salary']['to']}. "
+                                       f"Требования к кандидату: {i['snippet']['requirement']} "
+                                       f"Обязанности: {i['snippet']['responsibility']}")
+
+                else:
+                    result_info.append(f"Наименование вакансии: {i['name']}. "
+                                       f"Заработная плата({i['salary']['currency']}): {i['salary']['to']} - {i['salary']['from']}. "
+                                       f"Адрес офиса: {i['address']['city']} {i['address']['street']} {i['address']['building']}. "
+                                       f"Требования к кандидату: {i['snippet']['requirement']} "
+                                       f"Обязанности: {i['snippet']['responsibility']}")
+
+        return result_info
 
 
 hh_api_instance = HeadHunterApi("Python разработчик")
 hh_vacancy_interface = HHVacancyInterface(hh_api_instance)
 
 
-hh_vacancy_interface.fill_vacancy_list()
-hh_vacancy_interface.
 
 
 
