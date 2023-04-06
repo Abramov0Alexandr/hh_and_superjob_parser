eb4f45b1347140305a7fc449d91d7362b83f7e71
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-04-06 21:33:45 +0300

    Убрал демонстрационный код

--- src/HeadHunterAPIparse.py
+++ src/HeadHunterAPIparse.py
@@ -1,6 +1,6 @@
 import requests
 import datetime
-from AbstractCLS import ParseCLS
+from src.AbstractCLS import ParseCLS
 
 
 class HeadHunterApi(ParseCLS):
@@ -137,25 +137,3 @@ class HHVacancyInterface:
                                        f"Ссылка на вакансию: {i['alternate_url']}.")
 
         return result_info
-
-
-#: Создаем экземпляр класса АПИ. Можем через запятую передать ключевые слова в вакансии
-hh_api_instance = HeadHunterApi("python, Python разработчик")
-
-#: Создаем интерфейс для работы со списком полученных вакансий
-hh_vacancy_interface = HHVacancyInterface(hh_api_instance)
-
-#: Заполняем список вакансий полученной информацией
-hh_vacancy_interface.fill_vacancy_list()
-
-
-#: Можно получить список вакансий либо в виде списка, либо в виде словаря
-# print(hh_vacancy_interface.dict_of_vacancies)
-# print(hh_vacancy_interface.list_of_vacancies)
-
-#: Также можно вывести краткую информацию по всем полученным вакансиям
-# print(hh_vacancy_interface.show_vacancies)
-
-#: Можно получить более подробную информацию по конкретной вакансии
-# print(hh_vacancy_interface.get_full_information_by_id(78431357))
-
